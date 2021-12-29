5201af021309dd9deb01eb33a82c79f9d7a22a4c
Kasoma Ibrahim <kasomaibrahim@gmail.com>
2021-12-29 14:51:30 +0000

    Background Color Change

--- js/script.js
+++ js/script.js
@@ -84,6 +84,20 @@ const getRandomQuote = (quote) => {
 }
 
 /***
+ * `getRandomColor` function
+***/
+
+const getRandomColor = () => {
+  const r = Math.floor(Math.random() * 255 );
+  const g = Math.floor(Math.random() * 255 );
+  const b = Math.floor(Math.random() * 255 );
+
+  const rgb = `rgb(${r},${g},${b})`
+  return rgb
+}
+
+
+/***
  * `printQuote` function
 ***/
 const printQuote = () => {
